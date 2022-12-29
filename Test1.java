5b0995a5a0a8e656c1754cf9e282958902c59196
wpar2217 <73566936+wpar2217@users.noreply.github.com>
2022-12-29 11:58:22 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes") {
+			System.out.println("Eat Something....");
+		} else {
+			System.out.println("Do work.....");
+		}
+	}
+}
